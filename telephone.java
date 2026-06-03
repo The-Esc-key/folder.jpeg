36d52c4b2bea41148799108fcc2c441597d18e71
The-Esc-key <fieldsbran05@gmail.com>
2026-06-03 10:23:09 -0500

    update1

---
+++ telephone.java
@@ -0,0 +1,11 @@
+public class telephone {
+    public static void main (String[] args) {
+        int hit = 5892;
+        hit = addNum(hit);
+      
+        System.out.println(hit);
+    }  
+    public static int addNum(int y) {
+            return y + 482;
+        }
+}
